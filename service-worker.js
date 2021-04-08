320202c5b6a4b8e8114c21991f8cca748e3945ba
Taras Chaban <tarekchaban@gmail.com>
2021-04-09 02:55:41 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.c81f06623574362584dfb4081ad8971b.js"
 );
 
 self.addEventListener('message', (event) => {
